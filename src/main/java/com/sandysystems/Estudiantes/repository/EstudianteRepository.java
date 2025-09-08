2024953a5c01f4e6eacbd975c871a254213b1721
sandyH11 <SanHern11@gmail.com>
2025-09-08 02:57:26 -0600

    Primer commit: Carpeta Estudiante

---
+++ src/main/java/com/sandysystems/Estudiantes/repository/EstudianteRepository.java
@@ -0,0 +1,9 @@
+package com.sandysystems.Estudiantes.repository;
+
+import com.sandysystems.Estudiantes.entity.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
+}
