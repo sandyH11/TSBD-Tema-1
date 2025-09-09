d751ea237daea1c461fae05e0286512fad7d48ef
sandyH11 <sanhern11@gmail.com>
2025-09-09 09:52:27 -0600

    Delete MongoDB/Estudiantes directory

--- MongoDB/Estudiantes/src/main/java/com/sandysystems/Estudiantes/repository/EstudianteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sandysystems.Estudiantes.repository;
-
-import com.sandysystems.Estudiantes.entity.Estudiante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
-}
